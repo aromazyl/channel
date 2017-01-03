3d14fb35bea8c00ae1bda1f0f3881d0fe3f34532
aromazyl <zyl2336709@gmail.com>
2017-01-03 18:24:18 +0800

    update

--- message.h
+++ message.h
@@ -1,21 +1,41 @@
 #pragma once
 
 #include <functional>
+#include <unordered_map>
+#include <queue>
+#include "blob.h"
+
 namespace msg {
-enum MSG {
+
+enum MsgType {
+  STOP_ACK = -3;
+  SEND_ACK = -2;
+  RCV_ACK = -1;
+  DEFAULT = 0;
   RCV  = 1;
   SEND = 2;
   STOP = 3;
-  ACK  = 4;
 };
 
-class Message {
+struct Message {
+  MsgType type;
+  Blob blob;
 };
 
-typedef Function<void()>
+typedef Function<void(Message*)> MessageHandler;
+
 class Actor {
   public:
-  private:
+    Actor();
+    ~Actor() {}
 
+  public:
+    virtual void PreStart() = 0;
+    virtual void Receive(Message* msg) = 0;
+    virtual void PostExit() = 0;
+
+  protected:
+    std::unordered_map<MsgType, MessageHandler> handlers_;
+    std::queue<Message*> msgbox_;
 };
 }
