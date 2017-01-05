cce2c397b33c681066b6c87b1d26e10a2518d608
aromazyl <zyl2336709@gmail.com>
2017-01-05 21:24:42 +0800

    update

--- net/network.h
+++ net/network.h
@@ -8,21 +8,39 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include <string>
+#include <unordered_map>
 #include "../message.h"
 
 namespace net {
+
+enum NetType {
+  MPI = 0;
+  ZMQ = 1;
+  SOCKET = 2;
+};
+
+class ZMQ_NetWork;
+
 class NetWork {
-  private:
+  protected:
     NetWork() {}
     ~NetWork() {}
     NetWork(const NetWork&) {}
     void operator=(const NetWork&) {}
 
   public:
-    static NetWork* Get();
+    static NetWork* Get(const NetType& net_type);
+
+  public:
     virtual void Init(int* argc, char** argv) = 0;
     virtual void Finalize() = 0;
-
+    virtual void Bind(int rank) = 0;
+    virtual void Connect(int rank) = 0;
+    virtual void Send(int rank, const MessagePtr& msg) = 0;
+    virtual void Receive(int rank, MessagePtr& msg) = 0;
+    virtual int NodeNums() const = 0;
+    virtual void RegisterNetNode(int rank, const std::string& name) = 0;
 
   protected:
 };
---
+++ net/zmq_network.h
@@ -0,0 +1,29 @@
+/*
+ * zmq_network.h
+ * Copyright (C) 2017 zhangyule <zyl2336709@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef ZMQ_NETWORK_H
+#define ZMQ_NETWORK_H
+
+#include "./network.h"
+
+namespace net {
+class ZMQ_NetWork : public NetWork {
+  public:
+    void Init(int* argc, char** argv);
+    void Finalize();
+    void Bind(int rank);
+    void Connect(int rank);
+    void Send(int rank);
+    void Receive(int rank, MessagePtr& msg);
+    int NodeNums() const;
+    void RegisterNetNode(int rank, const std::string& name);
+  private:
+    std::unordered_map<int, std::string> node_table_;
+};
+}
+
+#endif /* !ZMQ_NETWORK_H */
