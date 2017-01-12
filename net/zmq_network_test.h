2f043b017c52f0a5e755b6db7953cafcc078949f
aromazyl <zyl2336709@gmail.com>
2017-01-12 20:45:14 +0800

    add testing

---
+++ net/zmq_network_test.h
@@ -0,0 +1,48 @@
+/*
+ * zmq_network_test.cc
+ * Copyright (C) 2017 zhangyule <zyl2336709@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#define public public
+#define private public
+#define protected public
+
+#include "zmq_network.h"
+
+#include <gtest/gtest.h>
+
+
+class ZMQNETWORKTesting : public ::testing::Test {
+  public:
+    void SetUp() {
+      zmq_network = new ZMQ_NetWork;
+    }
+  private:
+    ZMQ_NetWork* zmq_network;
+};
+
+TEST_F(ZMQNETWORKTesting, InitalZMQ_NETWORK_LOCAL) {
+}
+
+TEST_F(ZMQNETWORKTesting, FinalizeZMQ_NETWORK_LOCAL) {
+}
+
+TEST_F(ZMQNETWORKTesting, BindZMQ_NETWORK_LOCAL) {
+}
+
+TEST_F(ZMQNETWORKTesting, ConnectZMQ_NETWORK_LOCAL) {
+}
+
+TEST_F(ZMQNETWORKTesting, SendZMQ_NETWORK_LOCAL) {
+}
+
+TEST_F(ZMQNETWORKTesting, ReceiveZMQ_NETWORK_LOCAL) {
+}
+
+TEST_F(ZMQNETWORKTesting, NodeNumsZMQ_NETWORK_LOCAL) {
+}
+
+TEST_F(ZMQNETWORKTesting, RegisterNetNodeZMQ_NETWORK_LOCAL) {
+}
---
+++ test.cc
@@ -0,0 +1,15 @@
+/*
+ * test.cc
+ * Copyright (C) 2017 zhangyule <zyl2336709@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <gtest/gtest.h>
+#include "net/zmq_network_test.h"
+
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
