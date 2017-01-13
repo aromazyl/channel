662de9d1bfc6f44adfae8407df6122ea44ffaf15
aromazyl <zyl2336709@gmail.com>
2017-01-13 22:03:09 +0800

    add cmake files

---
+++ test/test.cc
@@ -0,0 +1,16 @@
+/*
+ * test.cc
+ * Copyright (C) 2017 zhangyule <zyl2336709@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <gtest/gtest.h>
+#include "net/zmq_network_test.h"
+#include "memory/memory_pool_test.h"
+
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
