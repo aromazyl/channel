3b0909b875fda852ee933c3d9c0308099a06884c
aromazyl <zyl2336709@gmail.com>
2017-03-26 16:29:59 +0800

    update

---
+++ protocols.h
@@ -0,0 +1,20 @@
+/*
+ * protocols.h
+ * Copyright (C) 2017 zhangyule <zyl2336709@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef PROTOCOLS_H
+#define PROTOCOLS_H
+
+#include "protocol/protocol.h"
+#include "protocol/comm_protocol.h"
+#include "protocol/pull_protocol.h"
+#include "protocol/push_protocol.h"
+#include "protocol/empty_protocol.h"
+#include "protocol/register_protocol.h"
+#include "protocol/heart_beat_protocol.h"
+#include "protocol/bcast_table_protocol.h"
+
+#endif /* !PROTOCOLS_H */
