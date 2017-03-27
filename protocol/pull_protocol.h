9f2f7f79393840759f0484614093e26830dd8faf
aromazyl <zyl2336709@gmail.com>
2017-03-27 18:53:51 +0800

    udpate

--- protocol/pull_protocol.h
+++ protocol/pull_protocol.h
@@ -12,7 +12,53 @@
 #include "protocol/protocol.h"
 
 namespace protocol {
+  using namespace msg;
 
+  template <>
+  struct Encoder<PULL> {
+    template <typename Key>
+    bool Apply(const Location& from, const Location& to, const std::vector<Key>& keys, Message** msg) {
+      if (!(*msg)) *msg = new Message;
+      (*msg)->type = PULL;
+      (*msg)->from = from;
+      (*msg)->to = to;
+      (*msg)->blob.resize(sizeof(Key) * keys.size());
+      (*msg)->blob.CopyFrom(&keys[0], sizeof(Key) * keys.size());
+      return true;
+    }
+  };
+
+  template <>
+  struct Decoder<PULL> {
+    template <typename Key>
+    bool Apply(const Message& msg, Location* from, Location* to, std::vector<Key>* keys) {
+      if (!from || !to) {
+        LOG(ERROR) << "from or to is empty. decode failure.";
+        return false;
+      }
+
+      *from = msg.from;
+      *to = msg.to;
+      if (!keys) {
+        LOG(ERROR) << "key is empty. decode failure.";
+        return false;
+      }
+
+      if (msg.blob.size() % sizeof(Key) != 0) {
+        LOG(ERROR) << base::StringPrintf("Informal key size:%d and blob size: %d\n",
+            msg.blob.size(), sizeof(Key));
+        return false;
+      }
+
+      keys->resize(msg.blob.size() / sizeof(Key));
+
+      for (int i = 0; i < msg.blob.size() / sizeof(Key); ++i) {
+        keys[i] = (int*)(msg.blob.data())[i];
+      }
+
+      return true;
+    }
+  };
 }
 
 #endif /* !PULL_PROTOCAL_H */
--- protocol/push_protocol.h
+++ protocol/push_protocol.h
@@ -8,6 +8,64 @@
 #ifndef PUSH_PROTOCOL_H
 #define PUSH_PROTOCOL_H
 
+#include "message.h"
+#include "protocol/protocol.h"
 
+namespace protocol {
+  using namespace msg;
+
+  // encode keys number, keys ..., values...
+  template <>
+  struct Encoder<PUSH> {
+    template <typename Key, typename Val>
+    bool Apply(const Location& from, const Location& to,
+        const std::vector<Key>& keys, const std::vector<Val>& vals, Message** msg) {
+      if (!(*msg)) *msg = new Message;
+      (*msg)->type = PUSH;
+      (*msg)->from = from;
+      (*msg)->to = to;
+      (*msg)->blob.resize(sizeof(Key) * keys.size() + sizeof(Val) * vals.size() + sizeof(int));
+      (*msg)->blob.CopyFrom(&keys[0], sizeof(Key) * keys.size());
+      return true;
+    }
+  };
+
+  template <>
+  struct Decoder<PUSH> {
+    template <typename Key, typename Val>
+    bool Apply(const Message& msg, Location* from,
+        Location* to, std::vector<Key>* keys, std::vector<Val>* vals) {
+      if (!from || !to) {
+        LOG(ERROR) << "from or to is empty. decode failure.";
+        return false;
+      }
+
+      *from = msg.from;
+      *to = msg.to;
+      if (!keys) {
+        LOG(ERROR) << "key is empty. decode failure.";
+        return false;
+      }
+
+      if (!vals) {
+        LOG(ERROR) << "val is empty. decode failure";
+        return false;
+      }
+
+      int num = (int*)(msg.blob.data())[0];
+      keys->resize(num);
+      vals->resize(num);
+      Key* k = (Key*)((char*)(msg.blob.data()) + sizeof(int));
+      Val* v = (Val*)((char*)k + sizeof(Key) * num);
+
+      for (int i = 0; i < num; ++i) {
+        keys[i] = k[i]; vals[i] = v[i];
+      }
+      return true;
+    }
+  };
+}
+
+#endif /* !PULL_PROTOCAL_H */
 
 #endif /* !PUSH_PROTOCOL_H */
